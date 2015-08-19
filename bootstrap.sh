a6c914eb8820422114b5404226d3513c777491f3
Kamil Dzieliński <brego.dk+d428c@gmail.com>
2015-08-19 14:57:34 +0200

    Add setup dir to ignore list

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 git pull
-function doIt() {
-	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "brew.sh" --exclude "readme.md" -av . ~
+function syncFiles() {
+  rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "setup" --exclude "readme.md" -av . ~
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt
+  syncFiles
 else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
-	echo
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt
-	fi
+  read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+  echo
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    syncFiles
+  fi
 fi
-unset doIt
+unset syncFiles
 source ~/.bash_profile
