e5930f3d397b44854df7cc12691fb8d36fae68a9
Kamil Dzieliński <brego.dk+d428c@gmail.com>
2015-09-24 23:15:52 +0200

    Add computer name change script.

---
+++ setup/set-computer-name.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `.osx` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+echo;
+read -p "Enter new computer name and press [ENTER]: " -r new_name;
+
+echo;
+read -p "Are you absolutely sure you want to use name $new_name? [Y/n] " -n 1 -r confirm;
+echo;
+if [[ ! $confirm =~ ^[Y]$ ]];
+  then
+    exit 1;
+  else
+    echo "Okay, proceeding.";
+fi;
+
+
+sudo scutil --set ComputerName "$new_name"
+sudo scutil --set HostName "$new_name"
+sudo scutil --set LocalHostName "$new_name"
+sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "$new_name"
+
+echo "Done.";
